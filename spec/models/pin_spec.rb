b48281469ad09848d168f0a2be0bdcec67469a95
Katie <katie@flutterflies.net>
2017-12-18 10:26:18 -0500

    add basic pin model

---
+++ spec/models/pin_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Pin, type: :model do
+  it 'is valid with a description' do
+    pin = Pin.new description: 'Description'
+    expect(pin).to be_valid
+  end
+end
