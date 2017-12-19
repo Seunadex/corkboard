2c42657b8674d60c2381d958384bff1cc073ca1a
Katie <katie@flutterflies.net>
2017-12-18 23:25:05 -0500

    simple user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with an email and password' do
+    user = User.new email: 'test@xample.com', password: 'lamepassword'
+    expect(user).to be_valid
+  end
 end
