4715d58a01e82799bc396cf44bcc0cdceb603b8c
Katie <katie@flutterflies.net>
2018-01-30 16:54:31 -0500

    Use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180112020527) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.integer "user_id"
     t.string "name"
